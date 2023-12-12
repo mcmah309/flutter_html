1b4324f0989b699909d9274ce2b7c39d3fd4627c
mcmah309 <dillonmcmahon79@gmail.com>
2023-12-12 18:27:36 -0500

    feat: Save mark overlay

--- lib/src/builtins/mark_built_in.dart
+++ lib/src/builtins/mark_built_in.dart
@@ -1,3 +1,4 @@
+import 'package:dart_tools/dart_tools.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_tools/flutter_tools.dart';
@@ -57,26 +58,26 @@ class MarkBuiltIn extends HtmlExtension {
         if (parentChildElement == element) break;
         parentShouldSkip++;
       }
-      _traverseAndAddStyle(
-          element.parent!, style, characterCount, parentShouldSkip);
+      _traverseAndAddStyle(element.parent!, style, characterCount, parentShouldSkip);
     }
   }
 
   void _traverseAndAddStyleDownInclusive(StyledElement element, Style style, Cell<int> characterCount, int skip) {
     if (characterCount.get() > 0) {
-      assert((element.node is dom.Text && element is TextContentElement) || (element.node is! dom.Text && element is!
-      TextContentElement), "The only Text nodes and TextContentElements should only be paired together");
+      assert(
+          (element.node is dom.Text && element is TextContentElement) ||
+              (element.node is! dom.Text && element is! TextContentElement),
+          "The only Text nodes and TextContentElements should only be paired together");
       if (element is TextContentElement) {
         String text = element.text;
         int length = text.length;
         // Single string non-empty elements are not counted. See [THE_HOLY_TRINITY.md] for more.
-        if(text == " "){
+        if (text == " ") {
           // Intentionally empty
         } else if (length > characterCount.get()) {
           final splitElement = element.split(characterCount.get());
           assert(splitElement.length == 2);
-          splitElement[0].style =
-              splitElement[0].style.copyOnlyInherited(style);
+          splitElement[0].style = splitElement[0].style.copyOnlyInherited(style);
           characterCount.sub(characterCount.get());
           return;
         } else {
@@ -86,36 +87,20 @@ class MarkBuiltIn extends HtmlExtension {
       }
     }
     for (int i = skip; i < element.children.length && characterCount.get() > 0; ++i) {
-      _traverseAndAddStyleDownInclusive(
-          element.children[i], style, characterCount, 0);
+      _traverseAndAddStyleDownInclusive(element.children[i], style, characterCount, 0);
     }
   }
 
   /// Adds a marker to the highlight that a comment can be attached to
   @override
   InlineSpan build(ExtensionContext context) {
-    // TODO replace with custom span that creates a floating widget
-    //double lineSpacing = context.styledElement!.style.lineHeight!.size!;
-    double letterHeight = context.styledElement!.style.fontSize!.value; //todo remove null check, log, return nada
-    double markerWidth = context.styledElement!.style.fontSize!.value;
+    double markerWidth = context.styledElement!.style.fontSize!.value; //todo remove null check, log, return nada
     double markerHeight = markerWidth;
     return WidgetSpan(
-        child: Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Positioned(
-          top: -(letterHeight + markerHeight / 2),
-          // position outside the stack
-          left: -markerWidth / 2,
-          child: Icon(
-            Icons.bookmark,
-            size: markerHeight,
-            color: const Color.fromARGB(255, 128, 0, 32), // burgundy
-            //shadows:  <Shadow>[Shadow(color: Colors.black, blurRadius: markerWidth/2), Shadow(color: Colors.white, blurRadius: markerWidth/2)]
-          ),
-        ),
-        const SizedBox.shrink()
-      ],
+        child: BookMark(
+      markerHeight: markerHeight,
+      markerWidth: markerWidth,
+      lineHeight: markerHeight,
     ));
     // return WidgetSpan(
     //   child: Stack(
@@ -135,6 +120,128 @@ class MarkBuiltIn extends HtmlExtension {
     // );
   }
 }
+
+class BookMark extends StatefulWidget {
+  const BookMark({
+    super.key,
+    required this.markerHeight,
+    required this.markerWidth,
+    required this.lineHeight,
+    this.child,
+  });
+
+  final double markerHeight;
+  final double markerWidth;
+  final double lineHeight;
+  final Widget? child;
+
+  @override
+  State<StatefulWidget> createState() => BookMarkState();
+}
+
+class BookMarkState extends State<BookMark> {
+  final OverlayPortalController _controller = OverlayPortalController();
+  final LayerLink layerLink = LayerLink();
+
+  GlobalKey mark = GlobalKey();
+  GlobalKey mark2 = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.endOfFrame.then((_) {
+      _controller.show();
+    });
+    return CompositedTransformTarget(
+      link: layerLink,
+      child: Container(
+        color: Colors.red,
+        height: 1,
+        width: 1,
+        key: mark,
+        child: OverlayPortal(
+          controller: _controller,
+          overlayChildBuilder: (BuildContext context) {
+            return Positioned(
+              width: widget.markerWidth,
+              child: CompositedTransformFollower(
+                offset: Offset(-widget.markerWidth / 2, -widget.lineHeight - widget.markerHeight / 2),
+                link: layerLink,
+                child: GestureDetector(
+                    onTap: () {
+                      Logging.w("taasgaed");
+                    },
+                    child: Icon(Icons.bookmark,
+                        size: widget.markerHeight,
+                        color: const Color.fromARGB(255, 128, 0, 32), // burgundy
+                        shadows: <Shadow>[
+                          Shadow(color: Colors.black, blurRadius: widget.markerWidth / 5, offset: const Offset(1, 1))
+                        ])),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// class BookMarkState extends State<BookMark> {
+//   final OverlayPortalController _controller = OverlayPortalController();
+//   final LayerLink layerLink = LayerLink();
+//
+//   GlobalKey mark= GlobalKey();
+//   GlobalKey mark2= GlobalKey();
+//
+//   OverlayEntry? entry;
+//
+//   @override
+//   void dispose() {
+//     entry?.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     WidgetsBinding.instance.endOfFrame.then((_){
+//       entry?.dispose();
+//       entry = OverlayEntry(builder: (BuildContext context) {
+//         RenderBox? box = mark.currentContext?.findRenderObject() as RenderBox?;
+//         if(box == null){
+//           Logging.e("This should never be null");
+//           return const SizedBox.shrink();
+//         }
+//         Offset position = box.localToGlobal(Offset.zero);
+//         return Positioned(
+//           left: position.dx,
+//           top: position.dy,
+//           child: CompositedTransformFollower(
+//             link: layerLink,
+//             child: GestureDetector(
+//                 onTap:() {
+//                   Logging.w("taasgaed");
+//                 },
+//                 child: Icon(Icons.bookmark,
+//                     size: widget.markerHeight,
+//                     color: const Color.fromARGB(255, 128, 0, 32), // burgundy
+//                     shadows: <Shadow>[
+//                       Shadow(color: Colors.black, blurRadius: widget.markerWidth / 5, offset: const Offset(1, 1))
+//                     ])),
+//           ),
+//         );
+//       });
+//       Overlay.of(context).insert(entry!);
+//     });
+//     return CompositedTransformTarget(
+//       link: layerLink,
+//       child: Container(
+//         color: Colors.red,
+//         height: 1,
+//         width: 1,
+//         key: mark,
+//       ),
+//     );
+//   }
+// }
 
 // class CustomMarkerIconPainter extends CustomPainter {
 //   @override
